318a321c98192d289d5d6012853b940e18ce95bd
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-28 19:11:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 chmod +x update.sh
 # Update RasPi Kernel
 sudo BRANCH=next rpi-update
+# Get some updates
+sudo apt-get update -y
+sudo apt-get upgrade -y
 # Enable USB gadget mode
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
