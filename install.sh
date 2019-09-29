463ffeac0436841f6957425b2955d9aeb0956454
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-28 23:04:15 -0400

    Update install.sh
    F

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 # Make every shell file executable
 chmod +x update.sh
 # Update RasPi Kernel
-sudo BRANCH=next rpi-update
+# sudo BRANCH=next rpi-update
 # Get some updates
 sudo apt-get update -y
 sudo apt-get upgrade -y
 # Enable USB gadget mode
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
+# Enable HID
+echo "g_hid" | sudo tee -a /etc/modules
 # Enable libcomposite driver
 sudo echo "libcomposite" | sudo tee -a /etc/modules
 # Enable GadgetFS
