526db467c8f58c7a670bb83648f6c90af8f38494
TheGreatRambler <aehart684351@gmail.com>
2019-10-07 20:32:52 -0400

    yes

--- source/GadgetFSApi/list.h
+++ source/GadgetFSApi/list.h
@@ -19,62 +19,50 @@
 #ifndef __LIST_H
 #define __LIST_H
 
-#include <stddef.h>		/* offsetof */
+#include <stddef.h> /* offsetof */
 
 struct usb_gadget_list_head {
-  struct usb_gadget_list_head *next, *prev;
+	struct usb_gadget_list_head *next, *prev;
 };
 
 /* copied and renamed from linux/include/linux/kernel.h */
-#define usb_gadget_container_of(ptr, type, member) ({			\
-      const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
-      (type *)( (char *)__mptr - offsetof(type,member) );})
+#define usb_gadget_container_of(ptr, type, member)                                                                                                                                                                                                                                                                                                                                                                                                                                                                 \
+	({                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             \
+		const __typeof__(((type*) 0)->member)* __mptr = (ptr);                                                                                                                                                                                                                                                                                                                                                                                                                                                     \
+		(type*) ((char*) __mptr - offsetof(type, member));                                                                                                                                                                                                                                                                                                                                                                                                                                                         \
+	})
 
-#define usb_gadget_list_entry(ptr, type, member)	\
-  usb_gadget_container_of(ptr, type, member)
+#define usb_gadget_list_entry(ptr, type, member) usb_gadget_container_of(ptr, type, member)
 
-#define usb_gadget_list_for_each_entry(pos, head, member)			\
-  for (pos = usb_gadget_list_entry((head)->next, typeof(*pos), member);	\
-       pos->member.next, &pos->member != (head);			\
-       pos = usb_gadget_list_entry(pos->member.next, typeof(*pos), member))
+#define usb_gadget_list_for_each_entry(pos, head, member) for (pos = usb_gadget_list_entry((head)->next, typeof(*pos), member); pos->member.next, &pos->member != (head); pos = usb_gadget_list_entry(pos->member.next, typeof(*pos), member))
 
-static inline void usb_gadget_init_list_head (struct usb_gadget_list_head *list)
-{
-  list->next = list;
-  list->prev = list;
+static inline void usb_gadget_init_list_head(struct usb_gadget_list_head* list) {
+	list->next = list;
+	list->prev = list;
 }
 
-static inline void __usb_gadget_list_add (struct usb_gadget_list_head *new,
-				   struct usb_gadget_list_head *prev,
-				   struct usb_gadget_list_head *next)
-{
-  next->prev = new;
-  new->next = next;
-  new->prev = prev;
-  prev->next = new;
+static inline void __usb_gadget_list_add(struct usb_gadget_list_head* new, struct usb_gadget_list_head* prev, struct usb_gadget_list_head* next) {
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
 }
 
-static inline void usb_gadget_list_add (struct usb_gadget_list_head *new,
-				 struct usb_gadget_list_head *head)
-{
-  __usb_gadget_list_add (new, head, head->next);
+static inline void usb_gadget_list_add(struct usb_gadget_list_head* new, struct usb_gadget_list_head* head) {
+	__usb_gadget_list_add(new, head, head->next);
 }
 
-static inline void usb_gadget_list_add_tail (struct usb_gadget_list_head *new,
-				      struct usb_gadget_list_head *head)
-{
-  __usb_gadget_list_add (new, head->prev, head);
+static inline void usb_gadget_list_add_tail(struct usb_gadget_list_head* new, struct usb_gadget_list_head* head) {
+	__usb_gadget_list_add(new, head->prev, head);
 }
 
-static inline void usb_gadget_list_del (struct usb_gadget_list_head *head)
-{
-  head->next->prev = head->prev;
-  head->prev->next = head->next;
+static inline void usb_gadget_list_del(struct usb_gadget_list_head* head) {
+	head->next->prev = head->prev;
+	head->prev->next = head->next;
 }
 
-static inline int usb_gadget_list_empty (struct usb_gadget_list_head *head)
-{
-  return head->next == head;
+static inline int usb_gadget_list_empty(struct usb_gadget_list_head* head) {
+	return head->next == head;
 }
 
 #endif
