f9554e7bc21f9a1074d0928d8751deecab2e556d
MonsterDruide1 <5958456@gmail.com>
2019-10-10 14:56:40 +0200

    Test

--- source/thirdParty/GadgetFSApi/usb-gadget.c
+++ source/thirdParty/GadgetFSApi/usb-gadget.c
@@ -118,8 +118,8 @@ static struct _usb_gadget_endpoint* find_ep0(struct usb_gadget_dev_handle* handl
 		}
 		if (!result) {
 			printf("ERROR: NO RESULT\n");
-			printf("ENTRY-NAME: ");
-			printf(entry->d_name);
+			printf("ENTRY-TYPE: ");
+			printf(entry->d_type);
 			printf("\n");
 			//cout << "ENTRY-OFF" << entry->d_off << "\n";
 			//cout << "ENTRY-RECLEN: " << entry->d_reclen << "\n";
