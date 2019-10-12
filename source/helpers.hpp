f306e8e568a0b262e942fa5f87cfa37db8830c4c
TheGreatRambler <aehart684351@gmail.com>
2019-10-12 18:14:07 -0400

    h

--- source/helpers.hpp
+++ source/helpers.hpp
@@ -84,11 +84,11 @@ std::vector<std::string> SplitString(const std::string& str, const std::string& 
 }
 
 
-std::string GetExecutablePath() {
+char* GetExecutablePath() {
 	char arg1[20];
 	char exepath[PATH_MAX + 1] = { 0 };
 
 	sprintf(arg1, "/proc/%d/exe", getpid());
 	readlink(arg1, exepath, 1024);
-	return std::string(exepath);
+	return exepath;
 }
