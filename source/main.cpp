3a1e539a2cb121d9fcb28f58dd9566c0d2523dd8
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-10-13 10:56:43 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -22,7 +22,9 @@ int main(int argc, char** argv) {
 	if (action == "update") {
 		// Run update
 		// Go to executable's path
-		chdir(GetExecutablePath());
+		char* exePath = GetExecutablePath();
+		puts(exePath);
+		chdir(exePath);
 		// Go back one folder
 		chdir("..");
 		puts("-----Starting Update-----");
