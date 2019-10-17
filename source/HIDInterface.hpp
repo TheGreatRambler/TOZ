3c5dbd1a624f110dda2b5f9e6109def89efb7b8f
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-10-17 13:56:54 -0400

    Update HIDInterface.hpp

--- source/HIDInterface.hpp
+++ source/HIDInterface.hpp
@@ -322,9 +322,13 @@ static void procontroller_event_cb(usb_gadget_dev_handle* handle, struct usb_gad
 }
 
 bool alreadyMounted() {
+	/*
 	struct stat buffer;
 	const std::string name = "/dev/gadget";
 	return (stat(name.c_str(), &buffer) == 0);
+	*/
+	// Just always mount
+	return false;
 }
 
 void StartGadget() {
