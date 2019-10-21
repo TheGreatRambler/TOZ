1bdd75e220db1c83a27508381a466364c13abc69
MonsterDruide1 <5958456@gmail.com>
2019-10-21 15:58:45 +0200

    More testing

--- source/Switch_HIDInterface.hpp
+++ source/Switch_HIDInterface.hpp
@@ -363,7 +363,7 @@ int StartGadget() {
 	// Configure ep0
 	send_size = (uint32_t) cp - (uint32_t) init_config;
 	
-	printf("Write-Return: "+write(fd,"Test-text",9));
+	printf("Write-Return: %d\n",write(fd,"Test-text",9));
 	
 	ret = write(fd, init_config, send_size);
 
