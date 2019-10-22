7ea34b941fde9f733abc3fe01d37e70024f7d445
MonsterDruide1 <5958456@gmail.com>
2019-10-22 12:15:10 +0200

    Now fixed?

--- test/test_descriptors.hpp
+++ test/test_descriptors.hpp
@@ -9,18 +9,18 @@
 #include "thirdParty/usbgadget.h"
 
 static struct usb_device_descriptor device_descriptor = {
-  .bLength = USB_DT_DEVICE_SIZE;
-	.bDescriptorType = USB_DT_DEVICE;
-	.bDeviceClass = USB_CLASS_COMM;
-	.bDeviceSubClass = 0;
-	.bDeviceProtocol = 0;
+  .bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bDeviceClass = USB_CLASS_COMM,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
 	// device_descriptor.bMaxPacketSize0 = 255; Set by driver
-	.idVendor = 0xAA; // My own id
-	.idProduct = 0xBB; // My own id
-	.bcdDevice = 0x0200; // Version
+	.idVendor = 0xAA, // My own id
+	.idProduct = 0xBB, // My own id
+	.bcdDevice = 0x0200, // Version
 	// Strings
-	.iManufacturer = STRINGID_MANUFACTURER;
-	.iProduct = STRINGID_PRODUCT;
-	.iSerialNumber = STRINGID_SERIAL;
-	.bNumConfigurations = 1; // Only one configuration
+	.iManufacturer = STRINGID_MANUFACTURER,
+	.iProduct = STRINGID_PRODUCT,
+	.iSerialNumber = STRINGID_SERIAL,
+	.bNumConfigurations = 1, // Only one configuration
 };
