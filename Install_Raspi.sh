308728d75da780d6a1634ec8756681046e3a04ec
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-10-30 18:40:45 -0400

    Update Install_Raspi.sh

--- Install_Raspi.sh
+++ Install_Raspi.sh
@@ -5,14 +5,8 @@ then
 	echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 fi
 
-# Add rpi-serial-console
-if ! [ -x "$(command -v rpi-serial-console)" ]
-then
-	# Script to turn the Raspi into a serial device and prevent it from being turned into a terminal
-	sudo wget https://raw.githubusercontent.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console && sudo chmod +x /usr/bin/rpi-serial-console
-	# Start now
-	sudo rpi-serial-console enable
-fi
+# Disable serial console (alternative to UI setup)
+raspi-config nonint do_serial 1
 
 # Finally, build the program
 make
